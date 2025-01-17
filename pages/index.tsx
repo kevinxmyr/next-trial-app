bebb003d8a116a2c742a64f6e80f3fe8b3665453
kevinxmyr <109541263+kevinxmyr@users.noreply.github.com>
2025-01-17 19:47:03 +0800

    added fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,25 +11,48 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function Home({ data }: { data: string }) {
+type Tdata = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
+export default function Home({ data }: { data: Tdata[] }) {
   if (!data) {
-    return <>
-      no data from getSServerSideProps.
-    </>
+    return <>no data from getSServerSideProps.</>;
   }
 
   return (
-    <div className="h-dvh flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-extrabold uppercase text-center">this is a server side rendered page</h1>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-extrabold uppercase text-center">
+        this is a server side rendered page
+      </h1>
+
+      <ul>
+        {data &&
+          data.map((d) => (
+            <li key={d.id}>
+              <p>TITLE: {d.title}</p>
+              <span>STATUS: {d?.completed?.toString()}</span>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
 
 export const getServerSideProps = async () => {
-  return {
-    props: {
-      data: "hello"
-    },
-  };
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.log("error has occured", error);
+  }
 };
